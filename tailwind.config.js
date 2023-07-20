10fd1ba3adaae128abb41049a1c4d5fbb953269e
codegino <carloginocatapang@gmail.com>
2023-07-20 21:00:13 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{jsx,tsx,mdx}',
+    './src/pages/*.tsx',
+    './src/app/**/*.{jsx,tsx,mdx}',
+    './src/app/*.tsx',
+    './src/components/**/*.{jsx,tsx}',
+    './src/modules/**/*.{jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
